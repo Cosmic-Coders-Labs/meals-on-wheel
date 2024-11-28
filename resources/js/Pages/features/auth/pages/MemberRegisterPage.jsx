5d06dcc7391dc516db5aae4ec8cf95cbeb288ec3
Su Myat Phyu <sumyatphyu2422004@gmail.com>
2024-11-28 22:51:47 +0630

    Adding new branch donate

--- resources/js/Pages/features/auth/pages/MemberRegisterPage.jsx
+++ resources/js/Pages/features/auth/pages/MemberRegisterPage.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Header from '../../../Components/Header';
+import Footer from '../../../Components/Footer';
 
 const MemberRegisterPage = () => {
   return (
-    <div>MemberRegisterPage</div>
-  )
+      <section className="flex flex-col min-h-screen">
+          <Header />
+          <div>MemberRegisterPage</div>
+          <Footer />
+      </section>
+  );
 }
 
 export default MemberRegisterPage
