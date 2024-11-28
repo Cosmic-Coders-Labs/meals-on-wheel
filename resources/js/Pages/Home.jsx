b0649c33334afe5a92f7f52ace7f3596deb81858
Su Myat Phyu <sumyatphyu2422004@gmail.com>
2024-11-28 15:56:22 +0630

    Changes to home.jsx

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div className="container">
-     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet quasi reiciendis, nostrum labore, dolor ab accusamus vero ipsum ducimus quam culpa nisi neque obcaecati. In aliquid ipsam expedita perspiciatis facilis.</p>
+     <p>This is the welcome page.</p>
     </div>
   )
 }
