19f3e4b1e49f821cea31a00deee5dd26366d0aca
Eaint Thiri Mon <eaintthimon3605@gmail.com>
2024-12-02 10:00:07 +0630

    Pulled Changes/Eaint

---
+++ resources/js/Pages/features/auth/pages/MemberRegisterPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from '../../../Components/Header';
+import Footer from '../../../Components/Footer';
+
+const MemberRegisterPage = () => {
+  return (
+      <section className="flex flex-col min-h-screen">
+          <Header />
+          <div>MemberRegisterPage</div>
+          <Footer />
+      </section>
+  );
+}
+
+export default MemberRegisterPage
