675293f2611987d6030cf440849206e3e10dfdf0
Eaint Thiri Mon <eaintthimon3605@gmail.com>
2024-12-02 15:56:49 +0630

    ...

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import Header from './Components/Header';
-import Footer from './Components/Footer';
+import React from "react";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 const Home = () => {
-  return (
-    <main className=' flex flex-col min-h-screen'>
-      <Header />
-      <h1 className=' container mx-auto'>Home</h1>
-      <Footer />
-    </main>
-  )
-}
+    return (
+        <main className=" flex flex-col min-h-screen">
+            <Header />
+            <h1 className=" container mx-auto">Home</h1>
+            <Footer />
+        </main>
+    );
+};
 
 export default Home;
 
