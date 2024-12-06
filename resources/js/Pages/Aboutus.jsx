acdc7c6add07e6c8d75de346b01baeb31ba48ea3
Myo Myint Myat <kairi31myomyat@gmail.com>
2024-12-06 20:02:34 +0630

    added dashboard admin

---
+++ resources/js/Pages/Aboutus.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+
+
+const Aboutus = () => {
+    return (
+        <main className="flex flex-col min-h-screen">
+            <Header/>
+            <Footer/>
+        </main>
+    );
+};
+
+export default Aboutus;
